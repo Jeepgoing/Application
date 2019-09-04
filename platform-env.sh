91eb6994e2fab57cefc449a6b145d087e0dad156
noitom <848269852@qq.com>
2019-09-04 18:41:12 +0800

    modify env

--- platform-env.sh
+++ platform-env.sh
@@ -4,7 +4,34 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+if [ -n "$ZSH_VERSION" ]; then
+	DIR="${(%):-%N}"
+	if [ $options[posixargzero] != "on" ]; then
+		setopt posixargzero
+		NAME=$(basename -- "$0")
+		setopt posixargzero
+	else
+		NAME=$(basename -- "$0")
+	fi
+else
+	DIR="${BASH_SOURCE[0]}"
+	NAME=$(basename -- "$0")
+fi
+
+if [ "X$NAME" "==" "Xplatform-env.sh" ]; then
+    echo "Source this file (do NOT execute it!) to set the Platform environment."
+    exit
+fi
+
+if uname | grep -q "MINGW"; then
+    win_build=1
+    PWD_OPT="-W"
+else
+    win_build=0
+    PWD_OPT=""
+fi
+
 # identify OS source tree root directory
-export PLATFORM_BASE=$( builtin cd "$( dirname "$DIR" )" && pwd ${PWD_OPT})
+export PLATFORM_BASE=$( builtin cd "$( dirname "$DIR"  )" > /dev/null && pwd ${PWD_OPT} )
 unset PWD_OPT
 
